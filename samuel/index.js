17f76ce10c0dd511190fdf04b9eea2d0a8a7a2ba
samueldhoore <55515834+samueldhoore@users.noreply.github.com>
2022-10-19 21:19:29 +0200

    Delete samuel directory

--- samuel/index.js
+++
@@ -1,7 +0,0 @@
-function changeBackgroundImage() {
-    if  (document.body.style.backgroundImage = "url('https://korenainthekitchen.com/wp-content/uploads/2011/02/white.jpg')"){
-        document.body.style.backgroundImage = "url('https://dl.dropbox.com/s/u4qg6w7rig06097/samuel.gif?dl=1')";
-  } else {
-    document.body.style.backgroundImage = "url('https://korenainthekitchen.com/wp-content/uploads/2011/02/white.jpg')";
-  }
-}
