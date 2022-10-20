088f24b1da1c75d4fe99e451e114d8e4ae9778c6
samueldhoore <55515834+samueldhoore@users.noreply.github.com>
2022-10-20 17:16:16 +0200

    sticky footer

--- bluai/v1/sticky_nav.js
+++ bluai/v1/sticky_nav.js
@@ -1,3 +1,8 @@
+/***************************************************************************************************************************************/
+/* DESKTOP VERSION */
+/***************************************************************************************************************************************/
+/* Sticky Top Nav */
+
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
@@ -8,5 +13,20 @@ function myFunction() {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
+  }
+}
+
+/* Sticky footer Nav */
+
+window.onscroll = function() {myFunction()};
+
+var footer = document.getElementById("footer");
+var sticky = footer.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    footer.classList.add("sticky")
+  } else {
+    footer.classList.remove("sticky");
   }
 }
