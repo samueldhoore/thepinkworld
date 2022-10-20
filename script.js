61a314b67630cc1f92620afcdae88001ace30dda
samueldhoore <55515834+samueldhoore@users.noreply.github.com>
2022-10-20 12:53:40 +0200

    reload

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 /* Random Link */
 /******************************************************************/
 
-/* document.onclick = reloadpage; */
+/* document.onclick = reloadpage;
 
 function reloadpage() {
     var sites = ['bluai', 'one', 'two'];
     var randomUrl = sites[Math.floor(Math.random() * sites.length )];
     window.open(randomUrl, "_self");
 }
+ */
+
+setTimeout(function reloadpage() {
+    var sites = ['bluai', 'one', 'two'];
+    var randomUrl = sites[Math.floor(Math.random() * sites.length )];
+    window.open(randomUrl, "_self");
+}, 1000)
