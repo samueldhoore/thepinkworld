0ac7b5fe1d02b115c893d7fa1f662a105d2695b6
samueldhoore <55515834+samueldhoore@users.noreply.github.com>
2022-10-20 14:22:44 +0200

    script sites

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 /* body.onload = setTimeout(random_link, 5000); */
 
 function random_link() {
-    var sites = ['bluai', 'one', 'two'];
+    var sites = ['bluai', 'bluai'];
     var randomUrl = sites[Math.floor(Math.random() * sites.length )];
     window.open(randomUrl, "_self");
 }
